1ccbfc5ea4406bd935d242fff8adf222001ad941
DiioBrando <olsaf21f12@gmail.com>
2025-06-11 20:01:34 +0500

    perf(auth): extract toast and redirect logic into reusable functions in useConfirm

--- src/features/auth/hooks/useConfirm.tsx
+++ src/features/auth/hooks/useConfirm.tsx
@@ -4,43 +4,47 @@ import {useMutation} from "@tanstack/react-query";
 import {postSignIn} from "@/features/auth/api/sign-in/postSignIn";
 import {postSignUp} from "@/features/auth/api/sign-up/postSignUp";
 import {useToast} from "@/shared/stores/toast";
+import {useRouter, useSearchParams} from "next/navigation";
+import {useUser} from "@/shared/stores/user";
 
 export const useConfirm = () => {
-    const mutation = useMutation;
+    const router = useRouter();
+    const searchParam = useSearchParams();
+    const callbackUrl = searchParam.get('callbackUrl') || '/';
     const {toast} = useToast();
-    const signInMutation = mutation({
+    const {setUser} = useUser();
+
+    const handleError = (error: any, title: string, description?: string) => {
+        toast({
+            title: title,
+            description: description + ` ${error.message}`,
+        });
+        console.error(error.message);
+    };
+    const handleSuccess = (data: any, title: string, description?: string) => {
+        toast({
+            title: title,
+            description: description,
+        });
+        setUser(data.data.user);
+        localStorage.setItem('accessToken', data.data.accessToken);
+        router.push(callbackUrl);
+    }
+
+    const signInMutation = useMutation({
         mutationFn: (data: SignInFormValues) => postSignIn(data),
-        onSuccess: (data) => {
-            toast({
-                title: "success",
-                description: `success sign-in`,
-            });
-            console.log(data);
-        },
-        onError: (error) => {
-            toast({
-                title: "error",
-                description: `${error.message}`,
-            });
-        }
+        onSuccess: (data) => handleSuccess(data, 'success', 'sign-in'),
+        onError: (error) => handleError(error, 'error'),
     });
-    const signUpMutation = mutation({
-        mutationFn: (data:SignUpFormValues) => postSignUp(data),
-        onSuccess: (data) => {
-            toast({
-                title: "success",
-                description: "success sign-up",
-            });
-        },
-        onError: (error) => {
-            toast({
-                title: "error",
-                description: `${error.message}`,
-            });
-        }
+
+    const signUpMutation = useMutation({
+        mutationFn: (data: SignUpFormValues) => postSignUp(data),
+        onSuccess: (data) => handleSuccess(data, 'success', 'sign-up'),
+        onError: (error) => handleError(error, 'error'),
     });
+
     return {
         signInMutation,
-        signUpMutation,
+        signUpMutation
     }
 }
