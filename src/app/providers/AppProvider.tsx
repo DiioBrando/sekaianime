d11e9ade36c0677d91a4b81cc0ebaba17521922f
DiioBrando <olsaf21f12@gmail.com>
2025-06-12 02:36:01 +0500

    feat(auth): auto-login if accessToken exists in localStorage

--- src/app/providers/AppProvider.tsx
+++ src/app/providers/AppProvider.tsx
@@ -1,10 +1,19 @@
 "use client";
-import {ReactNode, Suspense} from "react";
+import {ReactNode, Suspense, useEffect} from "react";
 import {ClientProvider} from "@/app/providers/query-client-provider";
 import {Toaster} from "@/app/providers/toast-provider";
 import {ThemeProvider} from "@/app/providers/theme-provider";
+import {useUser} from "@/shared/stores/user";
 
 export function AppProvider({children}: { children: ReactNode }) {
+    const {refreshUserToken} = useUser();
+
+    useEffect( () => {
+        if((typeof window !== 'undefined') && localStorage.getItem('accessToken')) {
+            refreshUserToken();
+        }
+    }, []);
+
     return (
         <Suspense fallback={<div>Loading...</div> as ReactNode}>
             <ClientProvider>
