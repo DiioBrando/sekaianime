7b4beea3231f6b166495a99a30b0c23eb36d03dd
DiioBrando <olsaf21f12@gmail.com>
2025-07-08 01:56:53 +0500

    feat: add useSearch hook

---
+++ src/shared/entities/aninlibria/hooks/useSearch.tsx
@@ -0,0 +1,40 @@
+import {useQuery} from "@tanstack/react-query";
+import {ConfigWithParams} from "@/shared/lib/callApi";
+import {SearchParams} from "@/shared/entities/aninlibria/model/AnilibriaApiParams";
+import {getSearch} from "@/shared/entities/aninlibria/api";
+
+const DEFAULT_PARAMS = {
+    filter: 'id,code,posters,names,type,season',
+}
+
+export const useSearch = ({config, queryKey}: { config?: ConfigWithParams<SearchParams>, queryKey?: any }) => {
+    const {params, ...propsConfig} = config || {};
+
+    const mergedParams: SearchParams = {
+        ...DEFAULT_PARAMS,
+        ...params
+    };
+
+    const {data, isLoading, isFetching, ...propsQuery} = useQuery({
+        queryFn: () => getSearch({
+            params: {
+                ...mergedParams,
+            },
+            ...propsConfig,
+        }),
+        queryKey: queryKey ?? ['title', 'search', params],
+        select: (data) => data.data,
+        enabled: !!params.search,
+    });
+
+    const showSkeleton = !!params.search && (isLoading || isFetching);
+    const showResults = !!params.search && !isFetching && data?.list.length;
+
+    return {
+        isLoading, isFetching,
+        showSkeleton,
+        showResults,
+        data,
+        ...propsQuery,
+    }
+}
